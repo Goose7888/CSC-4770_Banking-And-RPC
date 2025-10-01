6e1361592fc72e7ea82c241e5e8c8b7fa3bd99d9
Syndr <syndr@goose-games.com>
2025-09-30 21:28:30 -0500

    Update functions to take dollar amounts

--- client/client.cpp
+++ client/client.cpp
@@ -5,37 +5,52 @@
 int main(int argc, char** argv) {
     Arguments args = parseArgs(argc, argv);
 
+    if (args["operationMode"] == "credit") {
+        credit(args["acct_a"], args["amount"]);
+    }
+    else if (args["operationMode"] == "debit") {
+        debit(args["acct_a"], args["amount"]);
+    }
+    else if (args["operationMode"] == "transfer") {
+        transfer(args["acct_a"], args["acct_b"], args["amount"]);
+    }
+    else {
+        throw std::invalid_argument("Invalid operation mode!");
+    }
+
     return 0;
 }
 
-void credit(Account acct) {
-
+void credit(Account acct, u_int amount) {
+    // Use RPC to write to `int VB_credit(string account, int amount)`
 }
 
-void debit(Account acct) {
-
+void debit(Account acct, u_int amount) {
+    // Use RPC to write to `int VB_credit(string account, int amount)`
 }
 
-void transfer(Account acct_a, Account acct_b) {
-
+void transfer(Account acct_a, Account acct_b, u_int amount) {
+    // Use RPC to write to `int VB_credit(string account, int amount)`
 }
 
 Arguments parseArgs(int argc, char** argv) {
     Arguments args;
 
-    if (argc < 3) {
+    if (argc < 4) {
         throw std::invalid_argument("Not enough arguments!");
     }
 
     args["operationMode"] = argv[1];
     args["acct_a"] = argv[2];
 
-    if (args["operationMode"] == "transfer")
-    {
+    if (args["operationMode"] == "transfer") {
         if (argc < 4) {
             throw std::invalid_argument("Not enough arguments!");
         }
         args["acct_b"] = argv[3];
+    }
+    else {
+        args["amount"] = argv[3];
     }
 
     return args;
--- client/client.h
+++ client/client.h
@@ -5,21 +5,22 @@
 #include <string>
 #include <unordered_map>
 #include <stdexcept>
+#include <sys/types.h>
 
 //// Types:
 typedef std::string Account;
 typedef std::unordered_map<std::string, std::string> Arguments;
 
-//// Function Definitions:
+//// Function Declarations:
 
 // credits an account
-void credit(Account acct);
+void credit(Account acct, u_int amount);
 
 // debits an account
-void debit(Account acct);
+void debit(Account acct, u_int amount);
 
 // transfers money by creditting acct_a and debitting acct_b
-void transfer(Account acct_a, Account acct_b);
+void transfer(Account acct_a, Account acct_b, u_int amount);
 
 // parses cmd line parameters to determine operation
 Arguments parseArgs(int argc, char** argv);
